0f522279492c539bd55d3619266077c84dc1385b
MlMauriciolopes <60994723+MlMauriciolopes@users.noreply.github.com>
2020-09-07 22:43:51 -0300

    Add files via upload

---
+++ youtube/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="../script.js"></script>
+</head>
+<body>
+	<div id="corpo">
+		<div id="topo">
+			<div class="center">
+				<form>
+					<input type="text" name="search" autocomplete="off">
+				</form>
+
+				<div class="results">
+					<!--deixar em branco para exibir os resultados-->
+				</div><!--results-->
+
+			</div><!--center-->
+		</div><!--topo-->
+			<div id="content">
+			</div><!--content-->
+	</div><!--corpo-->
+</body>
+</html>
